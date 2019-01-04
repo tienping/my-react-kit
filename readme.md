44bcd9e5ef7372e2b09c51e16583d474c31d6688
TienPing <petwesley@gmail.com>
2019-01-04 16:43:02 +0800

    split dom from toolkit

---
+++ readme.md
@@ -0,0 +1,5 @@
+##to start:
+1. `npm i`
+2. `npm run build`
+3. `npm link`
+4. `npm publish --access public`
---
+++ src/index.js
@@ -0,0 +1,3 @@
+exports.printMsg = function() { console.log("This is a message from the demo package"); }
+exports.alertMsg = function() { alert("This is a message from the demo package"); }
+exports.sampleText = "This is tienping@my-react-toolkit sample text...";
